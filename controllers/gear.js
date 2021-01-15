c6f41a6aebc4b307acf95e28189427f17e2f163e
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-01-14 18:05:33 -0700

    added controller file

--- controllers/gear.js
+++ controllers/gear.js
@@ -3,10 +3,46 @@ const gearSwap = express.Router()
 
 const Gear = require('../models/gearswap.js')
 
-
-
 gearSwap.get('/', (req, res) => {
-  res.send('hello buddy')
+  Gear.find({}, (err, foundGear) => {
+    res.json(foundGear)
+  })
 })
+
+gearSwap.post('/', (req, res) => {
+  Gear.create(req.body, (err, createdGear) => {
+    Shirt.find({}, (err, foundGear) => {
+      res.json(foundGear)
+    })
+  })
+})
+
+gearSwap.put('/:id', (req, res) => {
+  Gear.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedShirt) => {
+      if (err) {
+        res.send(err)
+     } else {
+       Gear.find({}, (err, foundGear) => {
+         res.json(foundGear)
+       })
+     }
+    }
+  )
+})
+
+gearSwap.delete('/:id', (req, res) => {
+  Gear.findByIdAndRemove(req.params.id, (err, deletedGear) => {
+    Gear.find({}, (err, foundGear) => {
+      res.json(foundGear)
+    })
+  })
+})
+
+
+
 
 module.exports = gearSwap
